b058a7f3da1d5be6c69d002354be0ef147c7fc4d
turboignited <turboignited@gmail.com>
2018-05-30 20:57:57 +0100

    Update webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common');
-
+// inline-source-map is important to get traceable console output
 module.exports = merge(common, {
     mode: 'development',
     devtool: 'inline-source-map',
